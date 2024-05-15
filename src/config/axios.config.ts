f0293759c2bf6a51899efafa17f5fa1ef822c379
NourAldinSh <nouraldinshwehne2005@gmail.com>
2024-05-15 16:22:54 +0300

    change axios api link

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: "https://strapi-mdg8.onrender.com/api",
   timeout: 1000,
 });
 
